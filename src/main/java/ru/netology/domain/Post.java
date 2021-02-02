f6432efeb5ebcf8b4e0106d163f19125d80b9721
Elena Damdinova <elenadamdinova@gmail.com>
2021-02-02 20:05:41 +0800

    Версия 1

---
+++ src/main/java/ru/netology/domain/Post.java
@@ -0,0 +1,13 @@
+package ru.netology.domain;
+
+public class Post {
+    private int id;
+    private int date;
+    private String text;
+    private String imageUrl;
+    private LikesInfo likesInfo;
+    private CommentsInfo commentsInfo;
+    private RepostsInfo repostInfo;
+    private ViewsInfo viewsInfo;
+
+}
