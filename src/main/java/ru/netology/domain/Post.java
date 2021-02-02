b0f6a7aa09d562baf32f19c2efdbe9911976333e
Elena Damdinova <elenadamdinova@gmail.com>
2021-02-02 20:14:33 +0800

    Added classes and objects

--- src/main/java/ru/netology/domain/Post.java
+++ src/main/java/ru/netology/domain/Post.java
@@ -9,5 +9,21 @@ public class Post {
     private CommentsInfo commentsInfo;
     private RepostsInfo repostInfo;
     private ViewsInfo viewsInfo;
-
+    private int ownerId;
+    private int fromId;
+    private int createdId;
+    private int replyOwnerId;
+    private int replyPostId;
+    private int friendsOnly;
+    private PostType postType;
+    private PostSource postSource;
+    private Geo geo;
+    private int singerId;
+    private int canPin;
+    private int canDelete;
+    private int canEdit;
+    private int isPinned;
+    private int markedAsAds;
+    private boolean isFavorite;
+    private int postponedId;
 }
